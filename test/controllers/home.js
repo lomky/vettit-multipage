6759775aae66f7e5cf89961681e1921b6ea8bde7
Kathryn Tipton <kathryn.tipton@gmail.com>
2018-07-09 21:11:05 -0400

    Interrogating unlogged in pages in tests

--- test/controllers/home.js
+++ test/controllers/home.js
@@ -4,11 +4,14 @@ var request = require('supertest')(app);
 
 
 describe('Home Controller', function(){
-  it('should load the login page by default', function(done) {
+  it('should load the signup page by default', function(done) {
         request
             .get('/')
-            .expect(200, done)
-            //TODO interrogate content
+            .expect(200)
+            .expect(/<title>VolunteerCheck/)
+            .expect(/Learn More/)
+            .expect(/Signup/)
+            .end(done)
   })
   it('should load the org new page for logged in users without one', function(done) {
         //TODO create user, login user
@@ -27,8 +30,8 @@ describe('Home Controller', function(){
   it('should load the privacy page', function(done) {
         request
             .get('/privacy')
+            .expect(/<h2>Privacy Policy/)
+            .expect(/<title>Volunteer with/)
             .expect(200, done);
   })
 })
-
-// /privacy
