e20acdffc58972bc758dbdfa98d4004bdc3929dd
Kathryn Tipton <kathryn.tipton@gmail.com>
2018-07-31 15:30:40 -0400

    More skeleton test

--- test/controllers/home.js
+++ test/controllers/home.js
@@ -11,27 +11,34 @@ describe('Home Controller', function(){
             .expect(/<title>VolunteerCheck/)
             .expect(/Learn More/)
             .expect(/Signup/)
-            .end(done)
-  })
-  it('should load the org new page for logged in users without one', function(done) {
-        //TODO create user, login user
-        request
-            .get('/')
-            .expect(200, done);
-            //TODO interrogate content
-  })
-  it('should load the applications page for logged in users', function(done) {
-  	    //TODO create user, login user, create org
-        request
-            .get('/')
-            .expect(200, done);
-			//TODO interrogate content
-  })
+            .end(done);
+  });
   it('should load the privacy page', function(done) {
         request
             .get('/privacy')
             .expect(/<h2>Privacy Policy/)
             .expect(/<title>Volunteer with/)
             .expect(200, done);
-  })
-})
+  });
+  before('login', function(){
+    //TODO create user, login user
+    console.log("create user, login user");
+    it('should load the org new page for logged in users without one', function(done) {
+
+          request
+              .get('/')
+              .expect(200, done);
+              //TODO interrogate content
+    });
+    before('create org', function(){
+      //create org
+      console.log("create org");
+      it('should load the applications page for logged in users', function(done) {
+            request
+                .get('/')
+                .expect(200, done);
+          //TODO interrogate content
+      });
+    });
+  });
+});
